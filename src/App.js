4000a1a9b62599c89ff21bf09610d2fb7d9320e5
Jenny Roques <90016446+jenroques@users.noreply.github.com>
2022-08-29 13:15:25 -0500

    Updated files for basic production

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@
 import './App.css';
 import Home from './Home'
 import bootstrap from 'bootstrap';
-import { MDBAnimation } from 'mdb-react-ui-kit';
+
 
 function App() {
   return (
     <div className="App"  >
       <header className="App-header">
-        <MDBAnimation
-          reset={true}
-          tag='i'
-          animation='fade-in'
-          start='onLoad'
-          duration={2000}
-        >
-          <Home />
-        </MDBAnimation>
+
+        <Home />
+
 
       </header>
     </div>
